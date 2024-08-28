866673a8e35a5d3bbdbc6d3c0e64febc08af01e3
Nikita Anton Sergeevich <pydev097@gmail.com>
2024-08-28 18:07:27 +0500

    debug-toolbar added

--- app/urls.py
+++ app/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app.settings import DEBUG
 from django.contrib import admin
 from django.urls import include, path
+
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    urlpatterns += debug_toolbar_urls()
