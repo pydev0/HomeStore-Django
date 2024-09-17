79168b72795bb1c82ddf6b50a134e845d2a85d60
Nikita Anton Sergeevich <pydev097@gmail.com>
2024-09-17 15:20:03 +0500

    Login form and controller are ready

--- lesson.py
+++ lesson.py
@@ -1,38 +1,5 @@
-class Node:
+class TreeNode:
     def __init__(self, data):
+        self.left = None
+        self.right = None
         self.data = data
-        self.next = None
-        self.previous = None
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def append(self, data):
-        new_node = Node(data)
-
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-            return
-
-        self.tail.next = new_node
-        new_node.previous = self.tail
-        self.tail = new_node
-
-    def display(self):
-        current_node = self.head
-
-        while current_node:
-            print(current_node.data, end="<->")
-            current_node = current_node.next
-        print("None")
-
-
-ll = LinkedList()
-ll.append(1)
-ll.append(2)
-ll.append(3)
-ll.display()  # Вывод: 1 -> 2 -> 3 -> None
---
+++ users/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    username = forms.CharField()
+    password = forms.CharField()
+
+
+    #если не сделана форма в фронтенд части
+    # username = forms.CharField(
+    #     label='Имя',
+    #     widget=forms.TextInput(attrs={"autofocus": True,
+    #                                   'class': 'form-control',
+    #                                   'placeholder': 'Введите ваше имя пользователя',})
+    #     )
+
+    # password = forms.CharField(
+    #     label='Пароль',
+    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+    #                                     'class': 'form-control',
+    #                                     'placeholder': 'Введите ваше пароль',})
+    # )
