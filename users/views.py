604f80ecf9ef1e2be8a6d77a4563b047d88c84b7
Nikita Anton Sergeevich <pydev097@gmail.com>
2024-09-23 20:24:45 +0500

    Profile form, controller & notifications are ready

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
-from django.contrib import auth
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.shortcuts import redirect, render
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -16,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f'{username}, Вы успешно вошли в аккаунт')
                 return redirect('main:index')
     else:
         form = UserLoginForm()
@@ -34,6 +35,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f'{user.username}, Вы успешно зарегистрированы и  вошли в аккаунт')
             return redirect('main:index')
     else:
         form = UserRegistrationForm()
@@ -45,13 +47,26 @@ def registration(request):
     return render(request, "users/registration.html", context)
 
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль успешно обновлен')
+            return redirect('user:profile')
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         "title": "Home - Кабинет",
+        'form': form
     }
     return render(request, "users/profile.html", context)
 
 
+@login_required
 def logout(request):
+    messages.success(request, f'{request.user.username}, Вы успешно вышли из аккаунта')
     auth.logout(request)
     return redirect('main:index')
